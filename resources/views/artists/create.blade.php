2fa0265eb1def1be71c81cf58de31b7d2ee34a0c
K3rllon <francisco.silva9654@aluno.ce.gov.br>
2025-11-21 08:33:41 -0300

    chore(artists): remove espaço desnecessário no formulário de create

--- resources/views/artists/create.blade.php
+++ resources/views/artists/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Artista</title>
+    <title>Create Artist</title>
 </head>
 <body>
     <h1>Cadastrar Artista</h1>
