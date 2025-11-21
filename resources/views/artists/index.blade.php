950bbded00ea6ae0f91fea52a3b3d495a076b786
K3rllon <francisco.silva9654@aluno.ce.gov.br>
2025-11-21 08:32:53 -0300

    feat(artists): adiciona ações de edição na view index

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Artistas e Palestrantes - Kairos</title>
+    <title>Artist e Speaker - Kairos</title>
 </head>
 <body>
     <h1>Lista de Artistas e Palestrantes</h1>
@@ -25,7 +25,9 @@
                     <td>{{ $artist->name }}</td>
                     <td>{{ ucfirst($artist->type) }}</td>
                     <td>{{ ucfirst($artist->status) }}</td>
-
+                    <td>
+                        <a href="{{route('artists.edit', $artist->id)}}">Editar</a>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
